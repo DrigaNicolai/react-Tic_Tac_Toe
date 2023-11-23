bf27ebd7a876a17513e01f9ad610a72df4a22032
DrigaNicolai <90197647+DrigaNicolai@users.noreply.github.com>
2023-11-23 20:46:52 +0200

    Update index.js
    Some adjustments

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       history: [{
         squares: Array(9).fill(null),
@@ -68,6 +69,7 @@ class Game extends React.Component {
     }
 
     squares[i] = this.state.xIsNext ? 'X': 'O';
+    
     this.setState({
       history: history.concat([{
         squares: squares
